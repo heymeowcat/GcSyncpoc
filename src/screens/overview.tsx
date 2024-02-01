d3b0030255c3c7c679c13b0a292924c7befde4cf
Vidura Fernando <heyvidura@gmail.com>
2024-02-01 19:39:58 +0530

    feat: added firebase storage upload functions

--- src/screens/overview.tsx
+++ src/screens/overview.tsx
@@ -1,6 +1,7 @@
 import {useNavigation} from '@react-navigation/native';
 import {StackNavigationProp} from '@react-navigation/stack';
 import {YStack, TextArea, View, TextAreaFrame} from 'tamagui';
+import storage from '@react-native-firebase/storage';
 
 import {
   Container,
@@ -13,8 +14,10 @@ import {
 } from '../../tamagui.config';
 import {RootStackParamList} from '../navigation';
 import {Alert, ScrollView} from 'react-native';
-import {initDatabase, insertData} from '../utils/database';
+import {getData, initDatabase, insertData} from '../utils/database';
 import {useEffect, useState} from 'react';
+import _BackgroundTimer from 'react-native-background-timer';
+import moment from 'moment';
 
 type OverviewScreenNavigationProps = StackNavigationProp<
   RootStackParamList,
@@ -27,6 +30,13 @@ export default function Overview() {
 
   useEffect(() => {
     initDatabase();
+    checkAndUploadData();
+    const intervalId = _BackgroundTimer.setInterval(() => {
+      checkAndUploadData();
+    }, 24 * 60 * 60 * 1000);
+    return () => {
+      _BackgroundTimer.clearInterval(intervalId);
+    };
   }, []);
 
   const handleSave = async () => {
@@ -37,6 +47,47 @@ export default function Overview() {
     } catch (error) {
       console.error('Error saving data:', error);
       Alert.alert('Error', 'Failed to save data.');
+    }
+  };
+
+  const uploadData = async date => {
+    try {
+      const data = await getData(date);
+
+      if (data.length > 0) {
+        const jsonContent = JSON.stringify(data);
+        const fileName = `${date}.json`;
+
+        const storageRef = storage().ref(fileName);
+        await storageRef.putString(jsonContent);
+
+        console.log('File uploaded successfully:', fileName);
+      }
+    } catch (error) {
+      console.error('Error uploading data:', error);
+    }
+  };
+
+  const checkAndUploadData = async () => {
+    try {
+      const lastSaveDate = await getData('lastSave');
+
+      if (lastSaveDate.length === 0) {
+        const currentDate = moment().format('DD-MM-YYYY');
+        await insertData('lastSave', currentDate);
+      } else {
+        const lastSaveTime = moment(lastSaveDate[0].timestamp);
+        const currentTime = moment();
+        const timeDifference = currentTime.diff(lastSaveTime, 'milliseconds');
+
+        if (timeDifference >= 24 * 60 * 60 * 1000) {
+          const currentDate = moment().format('DD-MM-YYYY');
+          await uploadData(currentDate);
+          await insertData('lastSave', currentDate);
+        }
+      }
+    } catch (error) {
+      console.error('Error checking and uploading data:', error);
     }
   };
 
